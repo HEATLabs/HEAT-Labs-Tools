308a63f7c518b4435286554283b43e9b98230572
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2025-11-30 03:53:04 +0200

    updated site data updater tool with new repository

--- development-tools/Site-Data-Updater.py
+++ development-tools/Site-Data-Updater.py
@@ -23,6 +23,7 @@ def update_humans_txt_files():
         Path("../../HEAT-Labs-Images-Tournaments/site-data/humans.txt"),
         Path("../../HEAT-Labs-Models/site-data/humans.txt"),
         Path("../../HEAT-Labs-Mods/site-data/humans.txt"),
+        Path("../../HEAT-Labs-Archives/site-data/humans.txt"),
         Path("../../HEAT-Labs-Socials/site-data/humans.txt"),
         Path("../../HEAT-Labs-Sounds/site-data/humans.txt"),
         Path("../../HEAT-Labs-Static/site-data/humans.txt"),
